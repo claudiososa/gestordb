c0cb190d43f21b966450c1475aaf0f29761d0283
Claudia Bautista <clau.bautista06@gmail.com>
2017-04-25 12:47:52 -0300

    Se sacan las negritas de la descripcion de los documentos

--- includes/mod_cen/documentos/att.php
+++ includes/mod_cen/documentos/att.php
@@ -11,7 +11,7 @@
 <!-- contenido de categoria AYUDA DBMS-->
     <div class="panel-body">
       <div class="panel panel-success">
-        <div class="panel-heading"><h3 class="panel-title"><a href="documentacion/general/Tutorial-cambio-contraseña-DBMS.pdf" download="Tutorial-cambio-contraseña-DBMS.pdf"><span class="pull-right glyphicon glyphicon glyphicon-download-alt"></span><b>Cambio de Contraseña</a></h3>
+        <div class="panel-heading"><h3 class="panel-title"><a href="documentacion/general/Tutorial-cambio-contraseña-DBMS.pdf" download="Tutorial-cambio-contraseña-DBMS.pdf"><span class="pull-right glyphicon glyphicon glyphicon-download-alt"></span><b>Cambio de Contraseña</b></a></h3>
         </div>
 
         <div class="panel-body">Tutorial .PDF para el cambio de contraseña del usuario. </div>
@@ -21,7 +21,7 @@
 
 
               <div class="panel panel-success">
-                   <div class="panel-heading"><h3 class="panel-title"><a href="documentacion/general/Tutorial-creacion-informes-DBMS.pdf" download="Tutorial-creacion-informes-DBMS.pdf"><span class="pull-right glyphicon glyphicon glyphicon-download-alt"></span><b>Creación de Informes</a></h3>
+                   <div class="panel-heading"><h3 class="panel-title"><a href="documentacion/general/Tutorial-creacion-informes-DBMS.pdf" download="Tutorial-creacion-informes-DBMS.pdf"><span class="pull-right glyphicon glyphicon glyphicon-download-alt"></span><b>Creación de Informes</b></a></h3>
                    </div>
 
               <div class="panel-body">Tutorial .PDF para la creación de informes de Instituciones. </div>
@@ -29,7 +29,7 @@
 
 
               <div class="panel panel-success">
-                  <div class="panel-heading"><h3 class="panel-title"><a href="documentacion/general/Tutorial-edicionperfil-ubicacion-DBMS.pdf" download="Tutorial-edicionperfil-ubicacion-DBMS.pdf"><span class="pull-right glyphicon glyphicon glyphicon-download-alt"></span><b>Edición Perfil:Ubicación</a></h3>
+                  <div class="panel-heading"><h3 class="panel-title"><a href="documentacion/general/Tutorial-edicionperfil-ubicacion-DBMS.pdf" download="Tutorial-edicionperfil-ubicacion-DBMS.pdf"><span class="pull-right glyphicon glyphicon glyphicon-download-alt"></span><b>Edición Perfil:Ubicación</b></a></h3>
              </div>
 
              <div class="panel-body">Tutorial .PDF para actualizar datos del perfil del usuario incorporando las coordenadas desde Google Maps en el campo "Ubicación". </div>
@@ -37,14 +37,14 @@
 
 
       <div class="panel panel-success">
-                   <div class="panel-heading"><h3 class="panel-title"><a href="documentacion/ett/Tutorial-edicion-escuelas-DBMS.pdf" download="Tutorial-edicion-escuelas-DBMS.pdf"><span class="pull-right glyphicon glyphicon glyphicon-download-alt"></span><b>Edición de Escuelas</a></h3>
+                   <div class="panel-heading"><h3 class="panel-title"><a href="documentacion/ett/Tutorial-edicion-escuelas-DBMS.pdf" download="Tutorial-edicion-escuelas-DBMS.pdf"><span class="pull-right glyphicon glyphicon glyphicon-download-alt"></span><b>Edición de Escuelas</b></a></h3>
                    </div>
 
               <div class="panel-body">Tutorial .PDF para la modificación de los datos de escuelas (Nombre, Domicilio, Teléfonos, Nivel, Turnos, Localidad,etc.).</div>
               </div>
 
               <div class="panel panel-success">
-                   <div class="panel-heading"><h3 class="panel-title"><a href="documentacion/ett/Tutorial-modificacion-datos-director-DBMS.pdf" download="Tutorial-modificacion-datos-director-DBMS.pdf"><span class="pull-right glyphicon glyphicon glyphicon-download-alt"></span><b>Modificación del Director</a></h3>
+                   <div class="panel-heading"><h3 class="panel-title"><a href="documentacion/ett/Tutorial-modificacion-datos-director-DBMS.pdf" download="Tutorial-modificacion-datos-director-DBMS.pdf"><span class="pull-right glyphicon glyphicon glyphicon-download-alt"></span><b>Modificación del Director</b></a></h3>
                    </div>
 
               <div class="panel-body">Tutorial .PDF para la modificación o asignación de los datos del director/a de una institución.(Nombre, Apellido, Teléfonos, etc.).</div>
