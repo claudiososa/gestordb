2da0ccf5563b8dab3ee8eae361ea3a1bdb73855d
Claudia Bautista <clau.bautista06@gmail.com>
2017-04-27 00:22:07 -0300

    verificacion de menu documentos

--- includes/mod_cen/documentos/etj.php
+++ includes/mod_cen/documentos/etj.php
@@ -1,117 +1,22 @@
 <script type="text/javascript" src="includes/mod_cen/documentos/panel.js"></script>
+
+
 <div class="container">
   <!--categoria AYUDA DBMS-->
      <div class="row">
-		      <div class="col-md-4">
-			         <div class="panel panel-info">
-				           <div class="panel-heading">
-                        <span class="pull-right clickable"><i class="glyphicon glyphicon-chevron-down"></i></span>
-                        <span class="panel-title clickable"><h3 class="panel-title"><b>Ayuda DBMS</b></h3></span>
-                   </div>
-<!-- contenido de categoria AYUDA DBMS-->
-    <div class="panel-body">
-      <div class="panel panel-success">
-        <div class="panel-heading"><h3 class="panel-title"><a href="documentacion/general/Tutorial-cambio-contraseña-DBMS.pdf" download="Tutorial-cambio-contraseña-DBMS.pdf"><span class="pull-right glyphicon glyphicon glyphicon-download-alt"></span><b>Cambio de Contraseña</b></a></h3>
-        </div>
-
-        <div class="panel-body">Tutorial .PDF para el cambio de contraseña del usuario. </div>
-        </div>
-
-
-
-
-              <div class="panel panel-success">
-                   <div class="panel-heading"><h3 class="panel-title"><a href="documentacion/general/Tutorial-creacion-informes-DBMS.pdf" download="Tutorial-creacion-informes-DBMS.pdf"><span class="pull-right glyphicon glyphicon glyphicon-download-alt"></span><b>Creación de Informes</b></a></h3>
-                   </div>
-
-              <div class="panel-body">Tutorial .PDF para la creación de informes de Instituciones. </div>
-              </div>
-
-
-              <div class="panel panel-success">
-                  <div class="panel-heading"><h3 class="panel-title"><a href="documentacion/general/Tutorial-edicionperfil-ubicacion-DBMS.pdf" download="Tutorial-edicionperfil-ubicacion-DBMS.pdf"><span class="pull-right glyphicon glyphicon glyphicon-download-alt"></span><b>Edición Perfil:Ubicación</b></a></h3>
-             </div>
-
-             <div class="panel-body">Tutorial .PDF para actualizar datos del perfil del usuario incorporando las coordenadas desde Google Maps en el campo "Ubicación". </div>
-             </div>
-
-
-      <div class="panel panel-success">
-                   <div class="panel-heading"><h3 class="panel-title"><a href="documentacion/ett/Tutorial-edicion-escuelas-DBMS.pdf" download="Tutorial-edicion-escuelas-DBMS.pdf"><span class="pull-right glyphicon glyphicon glyphicon-download-alt"></span><b>Edición de Escuelas</b></a></h3>
-                   </div>
-
-              <div class="panel-body">Tutorial .PDF para la modificación de los datos de escuelas (Nombre, Domicilio, Teléfonos, Nivel, Turnos, Localidad,etc.).</div>
-              </div>
-
-              <div class="panel panel-success">
-                   <div class="panel-heading"><h3 class="panel-title"><a href="documentacion/ett/Tutorial-modificacion-datos-director-DBMS.pdf" download="Tutorial-modificacion-datos-director-DBMS.pdf"><span class="pull-right glyphicon glyphicon glyphicon-download-alt"></span><b>Modificación del Director</b></a></h3>
-                   </div>
-
-              <div class="panel-body">Tutorial .PDF para la modificación o asignación de los datos del director/a de una institución.(Nombre, Apellido, Teléfonos, etc.).</div>
-              </div>
-
-         </div>
-                   </div>
-              </div>
-
-    <!--Categoria Servidores escolares-->
-
-		     <div class="col-md-4">
-			        <div class="panel panel-info">
-				           <div class="panel-heading"><span class="pull-right clickable"><i class="glyphicon glyphicon-chevron-down"></i></span>
-                   <span class="panel-title clickable"><h3 class="panel-title"><b>Servidores Escolares</b></h3></span>
-                   </div>
-<!--contenido de categoria SERVIDORES ESCOLARES-->
-				      <div class="panel-body">
-                   <div class="panel panel-success">
-                   <div class="panel-heading"><h3 class="panel-title"><a href="documentacion/ett/servidores/Tutorial-generar-pendrive-clonezillaX64.pdf" download="Tutorial-generar-pendrive-clonezillaX64.pdf"><span class="pull-right glyphicon glyphicon glyphicon-download-alt"></span><b>Pendrive con CLONEZILLA X64.</b></a></h3>
-                   </div>
-
-              <div class="panel-body">Tutorial .PDF para generar un pendrive Booteable con CLONEZILLA X64 para el trabajo con los Servidores Escolares.</div>
-                   </div>
-
-                   <div class="panel panel-success">
-                   <div class="panel-heading"><h3 class="panel-title"><a href="documentacion/ett/servidores/Tutorial-reparacion-fallas-servicios-tdserver.pdf"download="Tutorial-reparacion-fallas-servicios-tdserver.pdf"><span class="pull-right glyphicon glyphicon glyphicon-download-alt"></span><b>Fallas y Servicios de TDSERVER</b></a></h3>
-                   </div>
-
-              <div class="panel-body">Tutorial .PDF de reparación de Fallas y Servicios en TDSERVER:<p></p><p><u><b><em>No se a podido encontrar a << services >>.</em> </p><p><em> << 172.16.0.2 no está respondiendo.>></em></b></u></p></div>
-                   </div>
-
-                   <div class="panel panel-success">
-                   <div class="panel-heading"><h3 class="panel-title"><a href="documentacion/ett/servidores/Tutorial-reparacion-inicio-servidor-debian.pdf" download="Tutorial-reparacion-inicio-servidor-debian.pdf"><span class="pull-right glyphicon glyphicon glyphicon-download-alt"></span><b>Reparación de Inicio de Servidor</b></a></h3>
-                   </div>
-
-              <div class="panel-body">Tutorial .PDF de reparación del sistema de archivos (File System) para solucionar el error de Inicio de un Servidor Escolar GNU/DEBIAN.</div>
-                   </div>
-
-
-				      </div>
-		          </div>
-	       </div>
-
 
 <!--categoria documentos administrativos-->
 
 <div class="col-md-4">
      <div class="panel panel-info">
-          <div class="panel-heading"><span class="pull-right clickable"><i class="glyphicon glyphicon-chevron-down"></i></span>
-          <span class="panel-title clickable"><h3 class="panel-title"><b>Documentos Administrativos</b></h3></span>
+          <div class="panel-heading" id="1"><span class="pull-right clickable"><i class="glyphicon glyphicon-chevron-down"></i></span>
+          <span class="panel-title clickable"><h3 class="panel-title"><b>Documentos Administrativos1</b></h3></span>
           </div>
 <!--contenido de categoria documentos administrativos-->
               <div class="panel-body">
-                <div class="panel panel-success">
-                <div class="panel-heading"><h3 class="panel-title"><a href="documentacion/administrativos/Acta-visita-modelo2017.docx" download="Acta-visita-modelo2017.docx"><span class="pull-right glyphicon glyphicon glyphicon-download-alt"></span><b>Acta de Visita 2017.</b></a></h3>
-                </div>
-
-           <div class="panel-body">Documento .docx de Acta de Visita modelo 2017.</div>
-                </div>
-
-                <div class="panel panel-success">
-                <div class="panel-heading"><h3 class="panel-title"><a href="documentacion/administrativos/Hoja-de-ruta2017.docx" download="Hoja-de-ruta2017.docx"><span class="pull-right glyphicon glyphicon glyphicon-download-alt"></span><b>Hoja de Ruta 2017.</b></a></h3>
-                </div>
-
-           <div class="panel-body">Documento .docx Hoja de Ruta.</div>
-                </div>
+                <?php
+                include("includes/mod_cen/documentos/pruebaetj.php");
+                ?>
 
 
               </div>
--- includes/mod_cen/documentos/panel.js
+++ includes/mod_cen/documentos/panel.js
@@ -1,9 +1,13 @@
 $(document).ready(function() {
 
 $('.panel-body').hide();
+
+
+
 //.on agrega la funcion evento click a ....
 $(document).on('click', '.panel-heading', 'span.clickable', function(){
   //var $this indica que posee un objeto jQuery.la seleccion se guarda en la variable
+
     var $this = $(this);
 //hasClass( className ): Determina si los elementos coincidentes tiene asignada la clase dada.
 	if(!$this.hasClass('.panel-collapsed')) {
@@ -13,12 +17,15 @@ $(document).on('click', '.panel-heading', 'span.clickable', function(){
 
 //slideup los elementos coincidentes tienn un movimiento deslizante hacia arriba .
   	$this.closest('.panel').find('.panel-body').slideDown();
+
 		$this.addClass('.panel-collapsed');
 		$this.find('i').removeClass('.glyphicon glyphicon-chevron-down').addClass('.glyphicon glyphicon-chevron-up');
 	} else {
 		$this.closest('.panel').find('.panel-body').slideUp();
-		$this.removeClass('.panel-collapsed');
+
+    $this.removeClass('.panel-collapsed');
 		$this.find('i').removeClass('.glyphicon glyphicon-chevron-up').addClass('.glyphicon glyphicon-chevron-down');
 	}
 });
+
 });
