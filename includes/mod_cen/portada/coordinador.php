01ebc7b86cd4ed1c10b026550ac066a613f6a432
Claudia Bautista <clau.bautista06@gmail.com>
2017-05-09 11:37:50 -0300

    Se modifica apariencia de portada ett-etj-att-coordinador pmi- coordinador conectar

--- includes/mod_cen/portada/coordinador.php
+++ includes/mod_cen/portada/coordinador.php
@@ -1,3 +1,4 @@
+	<script type="text/javascript" src="includes/mod_cen/documentos/panel.js"></script>
 <?php
 require_once("includes/mod_cen/clases/informe.php");
 require_once("includes/mod_cen/clases/persona.php");
@@ -52,9 +53,9 @@ echo '<div class="container">';
 	 if(mysqli_num_rows($buscar_alta)>0){
 	 	?>
 	 	<div class="panel panel-primary">
-	 		<div class="panel-heading">
-	 			<h4>Informes Prioridad Alta</h4>
-	 		</div>
+			<div class="panel-heading"><span class="panel-title clickable">
+				<h4>Informes Prioridad Alta<span class="pull-right clickable"><i class="glyphicon glyphicon-chevron-down"></i></span></h4></span>
+			</div>
 	 		<div class="panel-body">
 	 			<?php
 
@@ -113,8 +114,8 @@ echo '<div class="container">';
 	 if(mysqli_num_rows($buscar_media)>0){
 		?>
 		<div class="panel panel-primary">
-			<div class="panel-heading">
-				<h4>Informes Prioridad Media</h4>
+			<div class="panel-heading"><span class="panel-title clickable">
+				<h4>Informes prioridad Media<span class="pull-right clickable"><i class="glyphicon glyphicon-chevron-down"></i></span></h4></span>
 			</div>
 			<div class="panel-body">
 				<?php
@@ -175,8 +176,8 @@ echo '<div class="container">';
 		if(mysqli_num_rows($buscar_ett)>0){
 			?>
 			<div class="panel panel-primary">
-				<div class="panel-heading">
-					<h4>Informes por ETT</h4>
+				<div class="panel-heading"><span class="panel-title clickable">
+					<h4>Informes por ETT<span class="pull-right clickable"><i class="glyphicon glyphicon-chevron-down"></i></span></h4></span>
 				</div>
 				<div class="panel-body">
 					<?php
@@ -216,100 +217,6 @@ echo '<div class="container">';
 
 	if(mysqli_num_rows($resultado_ett_acargo)>0){
 		?>
-		<div class="panel panel-primary">
-			<div class="panel-heading">
-				<h4>Informes de ETJ a cargo</h4>
-			</div>
-			<div class="panel-body">
-				<?php
-
-				echo "<table id='informe_etj' class='table table-hover table-striped table-condensed tablesorter'>";
-				echo "<thead>";
-				echo "<tr>";
-				echo "<th>Apellido y Nombre</th>";
-				echo "<th>Cantidad</th>";
-				echo "</tr>";
-				echo "</thead>";
-
-				echo "<tbody>";
-
-				while ($fila=mysqli_fetch_object($resultado_ett_acargo)){
-					echo "<tr>";
-					echo "<td><a href='index.php?mod=slat&men=referentes&id=2&personaId=".$fila->personaId."&referenteId=".$fila->referenteId."'>".$fila->apellido.", ".$fila->nombre."</a></td>";
-					echo "<td>";
-					$informe_ett= new informe(null,null,$fila->referenteId);
-					$buscar_informe=$informe_ett->buscar();
-					$cantidad=mysqli_num_rows($buscar_informe);
-					//echo $cantidad;
-					echo '<a class="btn btn-success" href="?mod=slat&men=informe&id=6&referenteId='.$fila->referenteId.'">'.$cantidad.'</a>';
-
-					echo "</td>";
-					echo "</tr>";
-				}
-
-				echo "</tbody>";
-				echo "</table>";
-			}
-
-				 ?>
-			</div>
-		</div>
-<?php
-
-
-if(mysqli_num_rows($b_mis_informe)>0){
-	?>
-	<div class="panel panel-primary">
-		<div class="panel-heading">
-			<h4>Mis Informes (últimos 10)</h4>
-		</div>
-		<div class="panel-body">
-			<?php
-
-	echo "<table id='myTable' class='table table-hover table-striped table-condensed tablesorter'>";
-	echo "<thead>";
-	echo "<tr>";
-	echo "<th>Id</th>";
-	echo "<th>Título</th>";
-	echo "<th>Nº</th>";
-	echo "<th>Creado por...</th>";
-	echo "</tr>";
-	echo "</thead>";
-
-	echo "<tbody>";
-	while ($fila=mysqli_fetch_object($b_mis_informe)){
-
-		$escuela= new Escuela($fila->escuelaId);
-		$buscar_escuela= $escuela->buscar();
-		$dato_escuela= mysqli_fetch_object($buscar_escuela);
-
-		$referente = new Referente($_SESSION["referenteId"]);
-		$b_referente = $referente->buscar();
-
-		$dato_referente= mysqli_fetch_object($b_referente);
-
-		$persona = new Persona($dato_referente->personaId);
-
-		$b_persona = $persona->buscar();
-
-		$dato_persona=mysqli_fetch_object($b_persona);
-
-
-
-		echo "<tr>";
-		?>
-		<td><?php echo '<a href="index.php?mod=slat&men=informe&id=3&escuelaId='.$fila->escuelaId.'&informeId='.$fila->informeId.'">'.$fila->informeId.'</a>';?></td>
-		<td><?php echo '<a href="index.php?mod=slat&men=informe&id=3&escuelaId='.$fila->escuelaId.'&informeId='.$fila->informeId.'">'.$fila->titulo.'</a>';?></td>
-		<?php
-		echo "<td>".$dato_escuela->numero."</td>";
-		echo "<td>".$dato_persona->apellido."  ".$dato_persona->nombre."</td>";
-		echo "</td>";
-	}
-	echo "</tbody>";
-	echo "</table>";
-	?>
-</div>
-</div>
 <?php
 }
 echo "</div>";
@@ -319,8 +226,8 @@ echo "<div class='col-md-6'>";
 if(mysqli_num_rows($b_informe)>0){
 	?>
 	<div class="panel panel-primary">
-		<div class="panel-heading">
-			<h4>Ultimos informes creados</h4>
+		<div class="panel-heading"><span class="panel-title clickable">
+			<h4>Ultimos informes creados<span class="pull-right clickable"><i class="glyphicon glyphicon-chevron-down"></i></span></h4></span>
 		</div>
 		<div class="panel-body">
 			<?php
@@ -366,6 +273,102 @@ if(mysqli_num_rows($b_informe)>0){
 	?>
 </div>
 </div>
+
+
+<div class="panel panel-primary">
+	<div class="panel-heading"><span class="panel-title clickable">
+		<h4>Informes ETJ a Cargo<span class="pull-right clickable"><i class="glyphicon glyphicon-chevron-down"></i></span></h4></span>
+	</div>
+	<div class="panel-body">
+		<?php
+
+		echo "<table id='informe_etj' class='table table-hover table-striped table-condensed tablesorter'>";
+		echo "<thead>";
+		echo "<tr>";
+		echo "<th>Apellido y Nombre</th>";
+		echo "<th>Cantidad</th>";
+		echo "</tr>";
+		echo "</thead>";
+
+		echo "<tbody>";
+
+		while ($fila=mysqli_fetch_object($resultado_ett_acargo)){
+			echo "<tr>";
+			echo "<td><a href='index.php?mod=slat&men=referentes&id=2&personaId=".$fila->personaId."&referenteId=".$fila->referenteId."'>".$fila->apellido.", ".$fila->nombre."</a></td>";
+			echo "<td>";
+			$informe_ett= new informe(null,null,$fila->referenteId);
+			$buscar_informe=$informe_ett->buscar();
+			$cantidad=mysqli_num_rows($buscar_informe);
+			//echo $cantidad;
+			echo '<a class="btn btn-success" href="?mod=slat&men=informe&id=6&referenteId='.$fila->referenteId.'">'.$cantidad.'</a>';
+
+			echo "</td>";
+			echo "</tr>";
+		}
+
+		echo "</tbody>";
+		echo "</table>";
+	}
+
+		 ?>
+	</div>
+</div>
+<?php
+
+
+if(mysqli_num_rows($b_mis_informe)>0){
+?>
+<div class="panel panel-primary">
+	<div class="panel-heading"><span class="panel-title clickable">
+		<h4>Mis Informes (últimos 10)<span class="pull-right clickable"><i class="glyphicon glyphicon-chevron-down"></i></span></h4></span>
+	</div>
+<div class="panel-body">
+	<?php
+
+echo "<table id='myTable' class='table table-hover table-striped table-condensed tablesorter'>";
+echo "<thead>";
+echo "<tr>";
+echo "<th>Id</th>";
+echo "<th>Título</th>";
+echo "<th>Nº</th>";
+echo "<th>Creado por...</th>";
+echo "</tr>";
+echo "</thead>";
+
+echo "<tbody>";
+while ($fila=mysqli_fetch_object($b_mis_informe)){
+
+$escuela= new Escuela($fila->escuelaId);
+$buscar_escuela= $escuela->buscar();
+$dato_escuela= mysqli_fetch_object($buscar_escuela);
+
+$referente = new Referente($_SESSION["referenteId"]);
+$b_referente = $referente->buscar();
+
+$dato_referente= mysqli_fetch_object($b_referente);
+
+$persona = new Persona($dato_referente->personaId);
+
+$b_persona = $persona->buscar();
+
+$dato_persona=mysqli_fetch_object($b_persona);
+
+
+
+echo "<tr>";
+?>
+<td><?php echo '<a href="index.php?mod=slat&men=informe&id=3&escuelaId='.$fila->escuelaId.'&informeId='.$fila->informeId.'">'.$fila->informeId.'</a>';?></td>
+<td><?php echo '<a href="index.php?mod=slat&men=informe&id=3&escuelaId='.$fila->escuelaId.'&informeId='.$fila->informeId.'">'.$fila->titulo.'</a>';?></td>
+<?php
+echo "<td>".$dato_escuela->numero."</td>";
+echo "<td>".$dato_persona->apellido."  ".$dato_persona->nombre."</td>";
+echo "</td>";
+}
+echo "</tbody>";
+echo "</table>";
+?>
+</div>
+</div>
 <?php
 }
 echo "</div>";
@@ -395,30 +398,17 @@ $(document).ready(function()
 
 
 <div class="panel panel-primary">
-	<div class="panel-heading">
-		<h4>Tutoriales DBMS</h4>
+	<div class="panel-heading"><span class="panel-title clickable">
+		<h4>Documentación<span class="pull-right clickable"><i class="glyphicon glyphicon-chevron-down"></i></span></h4>
+	</span>
 	</div>
 	<div class="panel-body">
+		
+<?php
 
-				<div class="listap">
-				<p><img alt="documentos" src="img/iconos/carpetap.png"> Documentos</p>
+require_once("includes/mod_cen/documentos/coordinador-conectar.php");
+ ?>
 
-				<ul class="lista">
-						<li>Datos Colegio 2015 > Formato: Microsoft Excel > <a href="descarga/documentos/DatoColegio2015.xlsx" target="_blank">Descargar</a></li>
-				</ul>
-				</div>
-				<div class="listap">
-				<p><img alt="tutoriales" src="img/iconos/videotutorialp.png"> Video Tutoriales</p>
-
-				<ul class="lista">
-						<li>Carga de Datos de Colegio (nombre,telefono, etc. > Formato: MP4 > <a href="descarga/videotutorial-dbms/dbm-dato-colegio.mp4" download="dbm-dato-colegio.mp4" target="_blank" >Descargar</a></li><br>
-						<li>Carga de Datos de RTI  > Formato: MP4 > <a href="descarga/videotutorial-dbms/dbm-alta-rti.mp4" download="dbm-alta-rti.mp4" target="_blank" >Descargar</a></li><br>
-						<li>Carga de Dato de Autoridad (director, vicedirector, etc). > Formato: MP4 > <a href="descarga/videotutorial-dbms/dbm-director-dbms.mp4" download="dbm-director-dbms.mp4" target="_blank">Descargar</a></li><br>
-						<li>Asignación de Escuelas > Formato: MP4 > <a href="descarga/videotutorial-dbms/tutorial-asignar-escuelas.mp4" download="tutorial-asignar-escuelas.mp4" target="_blank">Descargar</a></li><br>
-				</ul>
-				</div>
-				<?php
-?>
 </div>
 </div>
 </div>
