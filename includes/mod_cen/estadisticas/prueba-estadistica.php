c96023db73768e82f82b757b3ffb0733785b4dd1
Claudia Bautista <clau.bautista06@gmail.com>
2017-05-23 11:57:48 -0300

    Graficos torta y barras incluidos

--- includes/mod_cen/estadisticas/prueba-estadistica.php
+++ includes/mod_cen/estadisticas/prueba-estadistica.php
@@ -2,20 +2,25 @@
 <script type="text/javascript" src="includes/mod_cen/estadisticas/Chart.min.js"></script>
 <script type="text/javascript" src="includes/mod_cen/estadisticas/Chart.PieceLabel.js-master/build/Chart.PieceLabel.min.js"></script>
 
+      <div class="container">
 
-    </head>
-    <body>
-       <div class="container">
-         <div class="btn btn-default" id="botonbarra">Mostrar en graficos de barra
-         </div>
 
+      <div class="btn btn-default" id="botonbarra" value="Graficos">Mostrar en graficos de barra
+      </div>
+      <div class="btn btn-default" id="botontorta" value="Graficos" style='display:none;'>Mostrar en graficos de torta
+      </div>
+
+
+ <div id="torta">
+
+<br><br>
       <div class="row">
           <div class="col-md-6">
             <div class="panel panel-primary">
               <div class="panel-heading">Instituciones con albergues:</div>
               <div class="panel-body">
             <canvas id="myChart" width="600" height="300"></canvas>
-          
+
               </div>
             </div>
 
@@ -55,12 +60,64 @@
 
   </div>
 </div>
+
 </div>
 </div>
+
+<div id="barra" style='display:none;'>
+
+<br><br>
+  <div class="row">
+      <div class="col-md-6">
+        <div class="panel panel-primary">
+          <div class="panel-heading">Instituciones con albergues:</div>
+          <div class="panel-body">
+        <canvas id="barra1"></canvas>
+<br><br>
+          </div>
+        </div>
+
+      </div>
+<div class="col-md-6">
+<div class="panel panel-primary">
+<div class="panel-heading">Conexion Internet</div>
+<div class="panel-body">
+<canvas id="barra2" width="600" height="300"></canvas>
+</div>
+</div>
+
+</div>
+</div>
+
+
+
+<div class="row">
+
+<div class="col-md-6">
+<div class="panel panel-primary">
+<div class="panel-heading">Instituciones con heladeras</div>
+<div class="panel-body">
+<canvas id="barra3" width="600" height="300"></canvas>
+</div>
+</div>
+
+</div>
+
+
+<div class="col-md-6">
+<div class="panel panel-primary">
+<div class="panel-heading">Instituciones con calefon</div>
+<div class="panel-body">
+<canvas id="barra4" width="600" height="300"></canvas>
+</div>
+
+</div>
+</div>
+
+</div>
+</div>
+</div>
+<script type="text/javascript" src="includes/mod_cen/estadisticas/botongrafico.js"></script>
 <script type="text/javascript" src="includes/mod_cen/estadisticas/scriptestadistica.js"></script>
-<!--
 <script type="text/javascript" src="includes/mod_cen/estadisticas/estadisticabarra.js">
 </script>
--->
-      </body>
-  </html>
