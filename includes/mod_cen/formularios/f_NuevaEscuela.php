9727c899f54bff76161d21f8cfebf04b8b709b0c
Claudia Bautista <clau.bautista06@gmail.com>
2017-06-28 12:16:49 -0300

    Apariencia y validacion de formulario nueva escuela

--- includes/mod_cen/formularios/f_NuevaEscuela.php
+++ includes/mod_cen/formularios/f_NuevaEscuela.php
@@ -1,63 +1,113 @@
+<script type="text/javascript" src="includes/mod_cen/formularios/js/form_nuevaEscuela.js"></script>
+<div class="container">
 
-<form action="index.php?mod=slat&men=escuelas&id=24" method="POST" >
-		<table>
-			<tr>
-				<th>Número:</th>
-				<td> <input size="40"  placeholder="solo números - 4 digitos" type="text" name="numero" pattern="[0-9]{4}">
-				</td>
-			</tr>
-			<tr>
-				<th>CUE</th>
-				<td> <input size="40" placeholder="solo números - 7 a 9 digitos"  type="text" name="cue" pattern="[0-9]{7,9}">
-				</td>
-			</tr>
-			<tr>
-				<th>Nombre</th>
-				<td> <input size="40" type="text" name="nombre" >
-				</td>
-			</tr>				
-			<tr>
-				<th>Domicilio</th>
-				<td> <input size="40" type="text" name="domicilio" />
-				</td>
-			</tr>
-			<tr>
-				<th>Teléfono</th>
-					 <td> <input placeholder="Nº Teléfono, solo números" size="40" title="Ingresar solo números" type="text" name="telefono" pattern="[0-9]{1,18}" />
-				</td>
-			</tr>
-			<tr>
-				<th>Nivel</th>
-			    <td> 
-					<select name="nivel">
-					         <option value="" label="Sin registrar">Sin registrar</option>	
-								<option value="Primaria Común" label="Primaria Común">Primaria Común</option>	
-								<option value="Primaria Especial" label="Primaria Especial">Primaria Especial</option>	
-								<option value="Secundaria Común" label="Secundaria Común">Secundaria Común</option>	
-								<option value="Secundaria Técnica" label="Secundaria Técnica"  >Secundaria Técnica</option>	
+<div class="panel panel-primary">
+	<div class="panel-heading">Nueva Escuela</div>
+	<div class="panel-body">
+				<form action="index.php?mod=slat&men=escuelas&id=24" method="POST" >
+
+					<div class="form-group">
+						<div class="col-md-12">
+							<label class="control-label" >Numero</label>
+
+						</div>
+						<div class="col-md-12">
+						<input class="form-control"size="40"  placeholder="solo números - 4 digitos" type="text" name="numero" pattern="[0-9]{4}" id="numEsc">
+
+						</div>
+
+					</div>
+
+					<div class="form-group">
+						<div class="col-md-12">
+							<label class="control-label" >CUE</label>
+
+						</div>
+						<div class="col-md-12">
+						<input class="form-control" size="40" placeholder="solo números - 7 a 9 digitos"  type="text" name="cue" pattern="[0-9]{7,9}" id="cueEsc">
+						</div>
+
+					</div>
+
+					<div class="form-group">
+						<div class="col-md-12">
+							<label class="control-label" >Nombre</label>
+
+						</div>
+						<div class="col-md-12">
+						<input class="form-control" size="40" type="text" name="nombre" placeholder="Nombre" id="nombreEsc">
+						</div>
+
+					</div>
+
+
+					<div class="form-group">
+						<div class="col-md-12">
+							<label class="control-label" >Domicilio</label>
+
+						</div>
+						<div class="col-md-12">
+					<input class="form-control" size="40" type="text" name="domicilio" placeholder="Domicilio">
+						</div>
+
+					</div>
+
+					<div class="form-group">
+						<div class="col-md-12">
+							<label class="control-label" >Teléfono</label>
+
+						</div>
+						<div class="col-md-12">
+					<input class="form-control" placeholder="Nº Teléfono, solo números" size="40" title="Ingresar solo números" type="text" name="telefono" pattern="[0-9]{1,18}" />
+						</div>
+
+					</div>
+
+
+					<div class="form-group">
+						<div class="col-md-12">
+							 <label class="control-label"><br>Nivel</label>
+						</div>
+						<div class="col-md-12">
+							 <select name="nivel" class="form-control" >
+								<option value="" label="Sin registrar">Sin registrar</option>
+								<option value="Primaria Común" label="Primaria Común">Primaria Común</option>
+								<option value="Primaria Especial" label="Primaria Especial">Primaria Especial</option>
+								<option value="Secundaria Común" label="Secundaria Común">Secundaria Común</option>
+								<option value="Secundaria Técnica" label="Secundaria Técnica"  >Secundaria Técnica</option>
 								<option value="Secundaria Rural" label="Secundaria Rural">Secundaria Rural</option>
 								<option value="ISFD" label="ISFD">ISFD</option>
 								<option value="IEM" label="IEM">IEM</option>
 								<option value="Capacitación" label="Capacitación">Capacitación</option>
-					</select>
-				</td>
-			</tr>
-			
-			<tr>
-				<th>Localidad</th>
-				<td> <select name="localidadId">
-				<?php 
+					 			</select>
+							</div>
+						</div>
+
+						<div class="form-group">
+						<div class="col-md-12">
+								<label class="control-label"><br>Localidad</label>
+						</div>
+						<div class="col-md-12">
+								<select name="localidadId" class="form-control" >
+														 <?php
 				while($fila = mysqli_fetch_object($resultado))
 				{
 					echo "<option value=".$fila->localidadId.">".$fila->nombre."</option>;"."\n";
 				}
 				?>
-				</select>
-				</td>
-			</tr>	
-			<tr>
-				<th></th>
-				<td> <input type="submit" name="guardar_escuela" value=" GUARDAR " />
-				</td>
-			</tr>
-		</table>
+									</select>
+						</div>
+						</div>
+
+						<div class="form-group" "input-group">
+								<button type="submit" class="btn btn-success" name="guardar_escuela" value="GUARDAR" id="btn_nuevaEsc">Guardar</button>
+						</div>
+
+
+</form>
+</div>
+
+</div>
+
+</form>
+</div>
