ec2486eced0179275a44131de43b5059786b221d
Alumno <alumno@huayra.(none)>
2017-06-28 19:42:05 -0300

    Script agregar escuela nueva funcionando

--- includes/mod_cen/escuelas/agregarEscuelas.php
+++ includes/mod_cen/escuelas/agregarEscuelas.php
@@ -6,7 +6,26 @@ include_once("includes/mod_cen/clases/localidades.php");
 
 if(isset($_POST["guardar_escuela"]) AND $_POST["nombre"]<>"" AND $_POST["cue"]<>"" ){
 
-echo" se guardo!!";
+
+
+$nuevaEscuela = new Escuela(null,'0001',$_POST["cue"],$_POST["numero"],$_POST["nombre"],$_POST["domicilio"],$_POST["nivel"],$_POST["localidadId"],null,$_POST["telefono"],null);
+
+
+$nuevo= $nuevaEscuela->agregar();
+
+//var_dump($nuevaEscuela)
+
+if ($nuevo == 1) {
+
+	echo "Se agrego con Exito la Escuela";
+	# code...
+}else{
+
+echo " Error al Guardar: ".$nuevo;
+
+}
+
+
 
 }else{
 
