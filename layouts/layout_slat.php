4898d3891c0eb523c61cd8757ad51720a171130f
Claudia Bautista <clau.bautista06@gmail.com>
2017-07-30 17:42:01 -0300

    apariencia de menu

--- layouts/layout_slat.php
+++ layouts/layout_slat.php
@@ -43,7 +43,7 @@ session_start();
 					 </article>
 					 <?php
 					 if ($_SESSION["nombre"]) {
-             echo '<nav class="navbar navbar-inverse" >';
+             echo '<nav class="navbar navbar-default" >';
                     if (file_exists($path_modulo2))
     		       		     include ($path_modulo2);
                     else
