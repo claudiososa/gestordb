6b6e746756fc6f13d04f33c484ca2af711ac700f
claudia <clau.bautista06@gmail.com>
2018-03-19 10:50:33 -0300

    Rti solucion

--- includes/mod_cen/clases/ajax/ajaxRti.php
+++ includes/mod_cen/clases/ajax/ajaxRti.php
@@ -1,17 +1,14 @@
 <?php
 include_once("../escuela.php");
-include_once("../referente.php");
 include_once("../rtixescuela.php");
 include_once("../persona.php");
 include_once("../rti.php");
 include_once("../maestro.php");
 
-$referenteId=$_SESSION['referenteId'];
-
-  if (isset($_POST['rti']))
+if (isset($_POST['rti']))
   {
     $list=array();
-    $escuela= new Escuela($_POST['escuela']);
+    $escuela= new Escuela($_POST["escuela"]);
     $fila=$escuela->buscarUnico();
     $rtix= new rtixescuela($fila->escuelaId);
     $buscar_rti=$rtix->buscar();
@@ -26,23 +23,20 @@ $referenteId=$_SESSION['referenteId'];
           $dato=mysqli_fetch_object($buscar_persona);
 
           $temporal=array(
-            'escuelaId'=>$fila->escuelaId,
+            'escuelaIdxxx'=>$fila->escuelaId,
             'numero'=>$fila->numero,
             'cue'=>$fila->cue,
-      			'nombre'=>$dato->apellido.", ".$dato->nombre,
+            'nombre'=>$dato->apellido.", ".$dato->nombre,
             'email'=>$dato->email,
             'cantidad'=>$cantidadRti,
             'telefonoM' =>$dato->telefonoM,
             'telefonoC' =>$dato->telefonoC,
             'turno' =>$filarti->turno
       		);
-
       		array_push($list,$temporal);
-
         }
-
       }
     $json = json_encode($list);
-    Maestro::debbugPHP($json);
+    //Maestro::debbugPHP($json);
     echo $json;
   }
