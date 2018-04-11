a27c40f556f920cad013f9a009a3dfbf50dc92f1
claudia <clau.bautista06@gmail.com>
2018-04-11 12:19:58 -0300

    cambios perfil /modificacion

--- includes/mod_cen/formularios/personaf.php
+++ includes/mod_cen/formularios/personaf.php
@@ -1,5 +1,170 @@
 <script type="text/javascript" src="includes/mod_cen/formularios/js/form_personaf.js"></script>
-			<?php
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="myModalLabel">Modificar datos</h4>
+      </div>
+      <div class="modal-body">
+        <!-- panel body inicio -->
+<?php  ?>
+        <form class="form-horizontal" action="index.php?men=personas&id=4" method="POST" >
+					<input type="hidden" name="personaId" value="<?php echo $personaId ?>"/>
+					<?php
+					if($_SESSION["tipo"]=="Coordinador"){
+						?>
+						<div class="form-group">
+						<label for="" class="col-sm-2 control-label">Apellidos</label>
+						<div class="col-sm-10">
+						   <input size="30" type="text" name="apellido" value="<?php echo $persona->getApellido()?>" placeholder="Ingrese Apellido" autofocus>
+						</div>
+						</div>
+						<div class="form-group">
+						<label for="" class="col-sm-2 control-label">Nombres</label>
+						<div class="col-sm-10">
+						  <input size="30" type="text" name="nombre" value="<?php echo $persona->getNombre()?>" placeholder="Ingrese Nombres">
+						</div>
+						</div>
+						<div class="form-group">
+						<label for="" class="col-sm-2 control-label">Foto de perfil</label>
+						<div class="col-sm-8">
+							<input type="file" id="exampleInputFile">
+						</div><div class="col-md-4">
+							<img src="img/iconos/pruebaFotoPerfil/foto-perfil.jpg" alt="..." class=" img-responsive img-circle" style="max-width:60%;">
+
+						</div>
+						</div>
+
+						<?php
+					}else{
+						?>
+						<div class="form-group">
+						<label for="" class="col-sm-2 control-label">Apellidos</label>
+						<div class="col-sm-10">
+						   <input size="30" type="text" name="apellido" value="<?php echo $persona->getApellido()?>" placeholder="Ingrese Apellido" autofocus>
+						</div>
+						</div>
+						<div class="form-group">
+						<label for="" class="col-sm-2 control-label">Nombres</label>
+						<div class="col-sm-10">
+						  <input size="30" type="text" name="nombre" value="<?php echo $persona->getNombre()?>" placeholder="Ingrese Nombres">
+						</div>
+						</div>
+						<div class="form-group">
+						<label for="" class="col-sm-2 control-label">Foto de perfil</label>
+						<div class="col-sm-8">
+							<input type="file" id="exampleInputFile">
+						</div>
+						<div class="col-md-4">
+							<br><img src="img/iconos/pruebaFotoPerfil/foto-perfil.jpg" alt="..." class=" img-responsive img-circle" style="max-width:60%;">
+
+						</div>
+						</div>
+						<?php
+					}
+
+					 ?>
+
+<div class="form-group">
+<label for="" class="col-sm-2 control-label">Dni</label>
+<div class="col-sm-10">
+   <input class="form-control" placeholder="Nº DNI sin puntos" size="30" title="Es un dato obligatorio - Ingresar sin puntos" type="text" name="dni" id="dni" pattern="[0-9]{7,8}" value="<?php echo $persona->getDni() ?>" required>
+</div>
+</div>
+<div class="form-group">
+<label for="" class="col-sm-2 control-label">Cuil</label>
+<div class="col-sm-10">
+  <input class="form-control" placeholder="Nº CUIL sin puntos, ni guiones" size="30" title="Ingresar sin guiones ni puntos"  type="text" name="cuil" id="cuil" pattern="[0-9]{11}" value="<?php echo $persona->getCuil()?>">
+</div>
+</div>
+<div class="form-group">
+<label for="" class="col-sm-2 control-label">Telefono Casa</label>
+<div class="col-sm-10">
+  <input class="form-control" placeholder="Nº Teléfono, solo números" size="30" title="Ingresar solo números" type="text" name="telefonoC" id="telefonoC" pattern="[0-9]{1,18}" value="<?php echo $persona->getTelefonoC() ?>">
+</div>
+</div>
+<div class="form-group">
+<label for="" class="col-sm-2 control-label">Celular</label>
+<div class="col-sm-10">
+<input class="form-control" placeholder="Nº Teléfono, solo números" size="30" title="Ingresar solo números" type="text" name="telefonoM" id="telefonoM" pattern="[0-9]{1,18}" value="<?php echo $persona->getTelefonoM() ?>"/>
+</div>
+</div>
+<div class="form-group">
+<label for="" class="col-sm-2 control-label">Email</label>
+<div class="col-sm-10">
+<input class="form-control" placeholder="Correo electrónico 1" title="Dato Obligatorio" size="30" type="email" name="email" id="correo1" value="<?php echo $persona->getEmail() ?>" required >
+</div>
+</div>
+<div class="form-group">
+<label for="" class="col-sm-2 control-label">Direccion</label>
+<div class="col-sm-10">
+<input class="form-control" placeholder="Dirección" size="30" type="text" name="direccion" value="<?php echo $persona->getDireccion() ?>">
+</div>
+</div>
+<div class="form-group">
+<label for="" class="col-sm-2 control-label">Facebook</label>
+<div class="col-sm-10">
+<input class="form-control" placeholder="Cuenta en facebook" size="30" type="text" name="facebook" value="<?php echo $persona->getFacebook()?>">
+</div>
+</div>
+<div class="form-group">
+<label for="" class="col-sm-2 control-label">Twitter</label>
+<div class="col-sm-10">
+<input class="form-control" placeholder="Cuenta en twitter" size="30" type="text" name="twitter" value="<?php echo $persona->getTwitter()?>">
+</div>
+</div>
+<div class="form-group">
+<label for="" class="col-sm-2 control-label">Localidad</label>
+<div class="col-sm-10">
+	<select class="form-control" 	name="localidadId">
+	<?php while($fila = mysqli_fetch_object($resultado))
+	{
+	if ($fila->localidadId == $persona->getLocalidadId()) {
+		echo "<option value=".$fila->localidadId." selected>".$fila->nombre."</option>;"."\n";
+	}
+	else {
+		echo "<option value=".$fila->localidadId.">".$fila->nombre."</option>;"."\n";
+	}
+	}
+	?>
+	</select>
+</div>
+</div>
+<div class="form-group">
+<label for="" class="col-sm-2 control-label">Codigo Postal</label>
+<div class="col-sm-10">
+<input class="form-control" placeholder="Código postal" size="30" type="text" name="cpostal" id="cpostal" value="<?php echo $persona->getCpostal()?>">
+</div>
+</div>
+<div class="form-group">
+<label for="" class="col-sm-2 control-label">Ubicacion</label>
+<div class="col-sm-10">
+<input class="form-control" placeholder="Coordenadas" size="30" type="text" name="ubicacion" value="<?php echo $persona->getUbicacion()?>">
+</div>
+</div>
+
+</form>
+<!-- panel body final -->
+      </div>
+      <div class="modal-footer">
+				<button type="button"id="boton1" class="btn btn-primary">Aplicar Cambios</button>
+        <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+
+				<!-- <input type='submit' id="boton1" class="btn btn-primary" value='Aplicar Cambios'> -->
+      </div>
+    </div>
+  </div>
+</div>
+
+<script type="text/javascript">
+$(document).ready(function() {
+
+});
+
+</script>
+
+			<!-- <?php
 			if($_SESSION["tipo"]=="Coordinador")
 				{
 					?>
@@ -174,4 +339,4 @@
 				<div class="col-md-3">
 					<input class="form-control" placeholder="Coordenadas" size="30" type="text" name="ubicacion" value="<?php echo $persona->getUbicacion()?>">
 				</div>
-			</div>
+			</div> -->
--- includes/mod_cen/personas/persona_editar.php
+++ includes/mod_cen/personas/persona_editar.php
@@ -1,5 +1,4 @@
 
-
 <?php
 		include_once('includes/mod_cen/clases/persona.php');
 		include_once('includes/mod_cen/clases/localidades.php');
@@ -13,6 +12,76 @@
 		$location=new Localidad();
 		$resultado=$location->buscar();
 		?>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-8 col-md-offset-2">
+
+					<div class="panel panel-default">
+
+						<div class="panel-body">
+							<!-- contenido de mi perfil -->
+							<h3 align="center">Datos Personales</h3>
+							<hr>
+							<!-- aqui empieza columnas-8 para datos 4 para foto -->
+							<div class="row">
+								<!-- columnas de info personal -->
+								<div class="col-md-8">
+
+										<h3><?php echo strtoupper ($persona->getApellido().' '.$persona->getNombre())?></h3>
+										<h4><img src="img/iconos/pruebaFotoPerfil/carnet-de-identidad (2).png" alt="">&nbsp&nbsp&nbsp<?php echo $persona->getDni() ?> / <?php echo $persona->getCuil()?></h4>
+										<h4><img src="img/iconos/pruebaFotoPerfil/llamada-smartphone.png" alt="">&nbsp&nbsp&nbsp<?php echo $persona->getTelefonoM() ?></h4>
+
+										<h4><img src="img/iconos/pruebaFotoPerfil/gmail (1).png" alt="">&nbsp&nbsp&nbsp<?php echo $persona->getEmail() ?></h4>
+										<?php
+										if ($persona->getTelefonoC() != "") {
+											echo'<h4><img src="img/iconos/pruebaFotoPerfil/gmail (1).png" alt="">&nbsp&nbsp&nbsp'.$persona->getTelefonoC().'</h4>';
+										}
+										// if ($persona->getEmail2() != "") {
+										// 	echo'<h4><img src="img/iconos/pruebaFotoPerfil/gmail (1).png" alt="">&nbsp&nbsp&nbsp'.$persona->getEmail2().'</h4>';
+										// }
+										if ($persona->getDireccion() != "") {
+											echo'<h4><img src="img/iconos/pruebaFotoPerfil/casa (5).png" alt="">&nbsp&nbsp&nbsp'.$persona->getDireccion().'</h4>';
+										}
+										if ($persona->getFacebook() != "") {
+											echo'<h4><img src="img/iconos/pruebaFotoPerfil/facebook (1).png" alt="">&nbsp&nbsp&nbsp'.$persona->getFacebook().'</h4>';
+										}
+										if ($persona->getTwitter() != "") {
+											echo'<h4><img src="img/iconos/pruebaFotoPerfil/gorjeo.png" alt="">&nbsp&nbsp&nbsp'.$persona->getTwitter().'</h4>';
+										}
+										if ($persona->getUbicacion() != "") {
+											echo'<h4><img src="img/iconos/pruebaFotoPerfil/ubicacion.png" alt="">&nbsp&nbsp&nbsp'.$persona->getUbicacion().'</h4>';
+										}
+
+
+										 ?>
+								<button type="button" name="button" data-toggle="modal" data-target="#myModal">Modificar</button>
+
+
+								</div>
+								<!-- columnas de foto -->
+								<br>
+								<div class="col-md-4">
+									<img src="img/iconos/pruebaFotoPerfil/foto-perfil.jpg" alt="..." class=" img-responsive img-circle" style="max-width:60%;">
+
+								</div>
+
+							</div><!-- cierre row -->
+
+
+						</div><!-- cierre panel-body -->
+
+					</div><!-- cierre panel-default -->
+
+
+				</div>
+
+			</div>
+
+
+		</div>
+
+<?php include_once('includes/mod_cen/formularios/personaf.php'); ?>
+<!--
 		<div class="container">
 		<form class="form-horizontal" action="index.php?men=personas&id=4" method="POST" >
 			<input type="hidden" name="personaId" value="<?php echo $personaId ?>"/>
@@ -31,4 +100,4 @@
 					<input type='submit' id="boton1" class="btn btn-primary" value='Aplicar Cambios'>
 				</div>
 			</div>
-		</div>
+		</div> -->
