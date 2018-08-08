d6f73e3066c8087c21e2eb05ceb862ef344140e2
Claudio <vicomser.claudio@gmail.com>
2018-08-08 12:50:22 -0300

    avance en clase compartepredio

--- includes/mod_cen/clases/CompartePredio.php
+++ includes/mod_cen/clases/CompartePredio.php
@@ -19,15 +19,32 @@ function __construct($id=NULL,$escuelaId=NULL,$predio=NULL,$referenteId=NULL)
 
 
 	public function agregar(){
-		
+		$bd=Conexion2::getInstance();
+		$sentencia = "INSERT INTO escuelaPredio (id,escuelaId,predio,referenteId)
+									VALUES (NULL,'".$this->escuelaId."','".$this->predio."','".$this->referenteId."')";
+		if ($bd->ejecutar($sentencia)) {
+			return $ultimoPredio=$bd->lastID();
+		}else{
+			return $sentencia."<br>"."Error al ejecutar la sentencia".$conexion->errno.":".$conexion->error;
+		}
 	}
 
+	public function editar(){
+		$bd=Conexion2::getInstance();
+		$sentencia = "UPDATE escuelaPredio SET escuelaId=$this->escuelaId,predio=$this->predio,referenteId=$this->referenteId
+									WHERE id=$this->id";
+		if ($bd->ejecutar($sentencia)) {
+			return $ultimoPredio=$bd->lastID();
+		}else{
+			return $sentencia."<br>"."Error al ejecutar la sentencia".$conexion->errno.":".$conexion->error;
+		}
+	}
 
 	public function buscar($limit=NULL)
 	{
 		$nuevaConexion=new Conexion();
 		$conexion=$nuevaConexion->getConexion();
-	  $sentencia="SELECT * FROM escuelapredio";
+	  $sentencia="SELECT * FROM escuelaPredio";
 
 		if($this->id!=NULL || $this->escuelaId!=NULL || $this->predio!=NULL)
 		{
