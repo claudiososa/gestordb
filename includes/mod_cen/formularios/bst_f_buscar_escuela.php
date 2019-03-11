f80a23f2cc0164ff6879cbbb7f1595792faac28c
claudiabautista <clau.bautista06@gmail.com>
2019-03-11 11:20:48 -0300

    Buscador escuelas old coordinador

---
+++ includes/mod_cen/formularios/bst_f_buscar_escuela.php
@@ -0,0 +1,37 @@
+<div class="card">
+	<div class="card-body">
+		<form action='' method='POST'>
+		  <div class="form-group">
+		    <label>Número</label>
+		    <input type="text" class="form-control"  name="numero" placeholder="Ingrese número" autofocus>
+
+		  </div>
+		  <div class="form-group">
+		    <label >Cue</label>
+		    <input type="text" class="form-control" name="cue" placeholder="Ingrese CUE">
+		  </div>
+			<div class="form-group">
+				<label >Nombre</label>
+				<input type="text" class="form-control" name="nombre" placeholder="Ingrese Nombre">
+			</div>
+			<div class="form-group">
+				<label >Departamento</label>
+				<?php
+				 $departamento= new Departamentos();
+				 $total=$departamento->getTotal();?>
+				<select class="form-control" name="localidadId" placeholder="Ingrese Nombre">
+					<option value="0">Ninguno</option>
+					<?php
+					for($val=2;$val<=$total;$val++) {
+						$departamento= new Departamentos($val);
+						$dato=$departamento->getDepartamento();
+						echo	"<option value='".$dato->getDepartamentoId()."' >".$dato->getDescripcion()."</option>";
+						}
+					 ?>
+				 </select>
+			</div>
+
+		  <button type="submit" class="btn btn-primary" value="Buscar">Buscar</button>
+		</form>
+	</div>
+</div>
